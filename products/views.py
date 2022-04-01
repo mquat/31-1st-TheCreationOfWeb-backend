8b39031f0a3bf671d1fb6246299d8c6d3d8eb613
sangahnhan <sangahn0524@gmail.com>
2022-04-01 18:58:12 +0900

    Add: products view, products urls RESTful 하게 구현

--- products/views.py
+++ products/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import *
+
+class CategoryView(View):
+    def get(self,request,category_id):
+        category = Category.objects.get(id=category_id)
+
+        products_list = [{
+            'name'  : product.name,
+            'price' : product.price,
+            'images': [image.image_url for image in product.picture_set.all()]
+        } for product in category.product_set.all()]
+
+        return JsonResponse({'product_list' : products_list} , status = 200)
+        
+class ProductView(View):
+    def get(self,request,category_id,product_id):
+        product = Category.objects.filter(id = category_id)[product_id-1]
+
+        product_information = {
+            'name'       : product.name,
+            'origin'     : product.origin,
+            'volume'     : product.volume,
+            'description': product.description,
+            'summary'    : product.summary,
+            'price'      : product.price,
+            'stock'      : product.stock,
+            'images'     : [image.image_url for image in product.picture_set.all()]
+        }
+
+        return JsonResponse({'product_information' : product_information} , status = 200)
