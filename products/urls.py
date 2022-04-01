2e68a6240b25eaa8171785b5b222e65922ebe83b
sangahnhan <sangahn0524@gmail.com>
2022-04-01 19:05:48 +0900

    Modify: fruitz_company.urls.py & products.urls.py 경로 연결

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from products.views import *
 
 urlpatterns = [
-    path('/categories/<int:category_id>',CategoryView.as_view()),
-    path('/categories/<int:category_id>/products/<int:product_id>',ProductView.as_view())
+    path('/<int:category_id>',CategoryView.as_view()),
+    path('/<int:category_id>/products/<int:product_id>',ProductView.as_view())
 ]
