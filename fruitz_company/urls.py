df6a40ddfce502f1ce732cea204b12b7dc6b5342
sangahnhan <sangahn0524@gmail.com>
2022-04-02 14:50:06 +0900

    Modify: url 경로 RESTful 하게 수정

--- fruitz_company/urls.py
+++ fruitz_company/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users', include('users.urls')),
-    path('categories',include('products.urls'))
+    path('products', include('products.urls'))
 ]
