b61c679bde3eb9da17d49c1b946fb9a8b622a42b
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-02 18:55:59 +0900

    Modify: gitignore 추가 및 products 앱 관련 내용 삭제

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,3 @@
 
 from django.urls import path
 
-from .views import CategoryView, ProductView 
-
-urlpatterns =[
-    path('/<int:category_id>', CategoryView.as_view()),
-    path('/<int:category_id>/products/<int:product_id>',ProductView.as_view())
-]
--- products/views.py
+++ products/views.py
@@ -1,38 +1,3 @@
-import json
-
-from django.views import View
-from django.http  import JsonResponse
-
-from .models import Category
-
-class CategoryView(View):
-    def get(self,request,category_id):
-        category = Category.objects.get(id=category_id)
-
-        result = [{
-            'name'     : product.name,
-            'price'    : product.price,
-            'image_url': [image.image_url for image in product.picture_set.all()]
-        } for product in category.product_set.all()]
-
-        return JsonResponse({'product_list':result}, status=200)
-
-class ProductView(View):
-    def get(self,request,category_id,product_id):
-        product = Category.objects.get(id=category_id).product_set.all()[product_id-1]
-        result = {
-            'name'        : product.name,
-            'origin'      : product.origin,
-            'volume'      : product.volume,
-            'description' : product.description,
-            'summary'     : product.summary,
-            'price'       : product.price,
-            'stock'       : product.stock,
-            'image_url'   : [image.image_url for image in product.picture_set.all()]
-            }  
-                
-        return JsonResponse({'product_info':result}, status=200)
-
 
 
 
