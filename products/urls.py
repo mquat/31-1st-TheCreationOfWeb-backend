3781d4317d8739c5eef4159d6d0edd5420537864
sangahnhan <sangahn0524@gmail.com>
2022-04-02 20:50:31 +0900

    Remove: ProductDetailView

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from products.views import ProductListView, ProductDetailView
+from products.views import ProductListView
 
 urlpatterns = [
-    path('/<int:product_id>',ProductDetailView.as_view()),
     path('',ProductListView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -15,23 +15,3 @@ class ProductListView(View):
         } for product in products]
 
         return JsonResponse({'product_list' : products_list} , status = 200)
-    
-class ProductDetailView(View):
-    def get(self,request,product_id):
-        try:
-            product = Product.objects.get(id = product_id)
-
-            product_detail = {
-                'name'        : product.name,
-                'origin'      : product.origin,
-                'volume'      : product.volume,
-                'description' : product.description,
-                'summary'     : product.summary,
-                'price'       : product.price,
-                'stock'       : product.stock,
-                'images'      : [image.image_url for image in product.picture_set.all()]  
-            }
-
-            return JsonResponse({'product_detail' : product_detail} , status = 200)
-        except Product.DoesNotExist:
-            return JsonResponse({'message' : 'INVALID_PRODUCT_ID'} , status = 404)
