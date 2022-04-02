f915cb2c26f09515cb21b483672385ea9b2d2d4b
sangahnhan <sangahn0524@gmail.com>
2022-04-02 19:56:34 +0900

    Modify: status code 수정

--- products/views.py
+++ products/views.py
@@ -5,20 +5,17 @@ from products.models import *
 
 class ProductListView(View):
     def get(self,request):
-        try:
-            category_id = request.GET.get('category_id' , None)
-            products    = Product.objects.filter(category_id = category_id)
+        category_id = request.GET.get('category_id' , None)
+        products    = Product.objects.filter(category_id = category_id)
 
-            products_list = [{
-                'name'  : product.name,
-                'price' : product.price,
-                'images': [image.image_url for image in product.picture_set.all()]
-            } for product in products]
+        products_list = [{
+            'name'  : product.name,
+            'price' : product.price,
+            'images': [image.image_url for image in product.picture_set.all()]
+        } for product in products]
 
-            return JsonResponse({'product_list' : products_list} , status = 200)
-        except Product.DoesNotExist:
-            return JsonResponse({'message' : 'INVALID_PRODUCT'} , status = 400)
-        
+        return JsonResponse({'product_list' : products_list} , status = 200)
+    
 class ProductDetailView(View):
     def get(self,request,product_id):
         try:
@@ -37,4 +34,4 @@ class ProductDetailView(View):
 
             return JsonResponse({'product_detail' : product_detail} , status = 200)
         except Product.DoesNotExist:
-            return JsonResponse({'message' : 'INVALID_PRODUCT_ID'} , status = 400)
+            return JsonResponse({'message' : 'INVALID_PRODUCT_ID'} , status = 404)
