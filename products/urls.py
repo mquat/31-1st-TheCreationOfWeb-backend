c9604203786a7af00dee1613f33d7bed30d9371e
sangahnhan <sangahn0524@gmail.com>
2022-04-02 14:30:52 +0900

    Modify: products view RESTful 하게 수정

--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from products.views import *
-
-urlpatterns = [
-    path('/<int:category_id>',CategoryView.as_view()),
-    path('/<int:category_id>/products/<int:product_id>',ProductView.as_view())
-]
--- products/views.py
+++ products/views.py
@@ -3,31 +3,39 @@ from django.http  import JsonResponse
 
 from products.models import *
 
-class CategoryView(View):
-    def get(self,request,category_id):
-        category = Category.objects.get(id=category_id)
+class ProductListView(View):
+    def get(self,request):
+        try:
+            category_id = request.GET.get('category_id' , None)
+            category    = Category.objects.get(id = category_id)
+            products    = category.product_set.all()
 
-        products_list = [{
-            'name'  : product.name,
-            'price' : product.price,
-            'images': [image.image_url for image in product.picture_set.all()]
-        } for product in category.product_set.all()]
+            products_list = [{
+                'name'  : product.name,
+                'price' : product.price,
+                'images': [image.image_url for image in product.picture_set.all()]
+            } for product in products]
 
-        return JsonResponse({'product_list' : products_list} , status = 200)
+            return JsonResponse({'product_list' : products_list} , status = 200)
+        except Category.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_CATEGORY_ID'} , status = 400)
         
 class ProductView(View):
-    def get(self,request,category_id,product_id):
-        product = Category.objects.get(id = category_id).product_set.all()[product_id-1]
+    def get(self,request,product_id):
+        try:
+            product = Product.objects.get(id = product_id)
 
-        product_information = {
-            'name'       : product.name,
-            'origin'     : product.origin,
-            'volume'     : product.volume,
-            'description': product.description,
-            'summary'    : product.summary,
-            'price'      : product.price,
-            'stock'      : product.stock,
-            'images'     : [image.image_url for image in product.picture_set.all()]  
-        }
+            product_information = {
+                'name'        : product.name,
+                'origin'      : product.origin,
+                'volume'      : product.volume,
+                'description' : product.description,
+                'summary'     : product.summary,
+                'price'       : product.price,
+                'stock'       : product.stock,
+                'images'      : [image.image_url for image in product.picture_set.all()]  
+            }
 
-        return JsonResponse({'product_information' : product_information} , status = 200)
+            return JsonResponse({'product_information' : product_information} , status = 200)
+        except Product.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_PRODUCT_ID'} , status = 400)
