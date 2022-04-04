629341f744d58b1f8c6d41a5ace90a03be06346c
sangahnhan <sangahn0524@gmail.com>
2022-04-04 16:01:15 +0900

    offset 과 limit 기준으로 pagination

--- products/views.py
+++ products/views.py
@@ -5,30 +5,23 @@ from products.models import *
 
 class ProductListView(View):
     def get(self,request):
-        try:
-            category_id = request.GET.get('category_id' , None)
-            page_id     = int(request.GET.get('page_id' , None))
+        category_id = int(request.GET.get('category_id' , None))
+        offset = request.GET.get('offset' , None)
+        limit = request.GET.get('limit' , None)
 
-            if category_id == None:
-                return JsonResponse({'message' : 'QUERY_STRING_ERROR'} , status = 404)
-            if page_id < 0:
-                return JsonResponse({'message' : 'PAGE_NUMBER_ERROR'} , status = 404)
+        if category_id <= 0:
+            return JsonResponse({'message' : 'INVALID_CATEGORY_ID'} , status = 404)
 
-            PAGE_SIZE   = 10
-            offset      = (page_id - 1) * PAGE_SIZE
-            limit       = page_id * PAGE_SIZE
+        # offset      = (page_id - 1) * PAGE_SIZE
+        # limit       = page_id * PAGE_SIZE
 
-            products    = Product.objects.filter(category_id = category_id)[offset:limit]
+        products    = Product.objects.filter(category_id = category_id)#[offset:limit]
 
-            products_list = [{
-                'id'     : product.id,
-                'name'   : product.name,
-                'price'  : product.price,
-                'images' : [image.image_url for image in product.picture_set.all()]
-            } for product in products]
+        products_list = [{
+            'id'     : product.id,
+            'name'   : product.name,
+            'price'  : product.price,
+            'images' : [image.image_url for image in product.picture_set.all()]
+        } for product in products]
 
-            return JsonResponse({'product_list' : products_list} , status = 200)
-        except ValueError:
-            return JsonResponse({'message' : 'QUERY_STRING_ERROR'} , status = 404)
-        except TypeError:
-            return JsonResponse({'message' : 'QUERY_STRING_ERROR'} , status = 404)
+        return JsonResponse({'product_list' : products_list} , status = 200)
