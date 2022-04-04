f9d5a06b78a7a87b5b5c2193f7bde39d5c8f0df6
sangahnhan <sangahn0524@gmail.com>
2022-04-04 11:48:06 +0900

    Add: ProductListView 쿼리스트링 에러메세지 생성

--- products/views.py
+++ products/views.py
@@ -5,19 +5,25 @@ from products.models import *
 
 class ProductListView(View):
     def get(self,request):
-        category_id = request.GET.get('category_id' , None)
-        page_id     = int(request.GET.get('page_id' , 1) or 1)
+        try:
+            category_id = request.GET.get('category_id' , None)
+            page_id     = int(request.GET.get('page_id' , 1) or 1)
 
-        PAGE_SIZE   = 10
-        offset      = (page_id - 1) * PAGE_SIZE
-        limit       = page_id * PAGE_SIZE
+            if page_id < 0:
+                return JsonResponse({'message' : 'PAGE_NUMBER_ERROR'} , status = 404)
 
-        products    = Product.objects.filter(category_id = category_id)[offset:limit]
+            PAGE_SIZE   = 10
+            offset      = (page_id - 1) * PAGE_SIZE
+            limit       = page_id * PAGE_SIZE
 
-        products_list = [{
-            'name'  : product.name,
-            'price' : product.price,
-            'images': [image.image_url for image in product.picture_set.all()]
-        } for product in products]
+            products    = Product.objects.filter(category_id = category_id)[offset:limit]
 
-        return JsonResponse({'product_list' : products_list} , status = 200)
+            products_list = [{
+                'name'  : product.name,
+                'price' : product.price,
+                'images': [image.image_url for image in product.picture_set.all()]
+            } for product in products]
+
+            return JsonResponse({'product_list' : products_list} , status = 200)
+        except ValueError:
+            return JsonResponse({'message' : 'QUERY_STRING_ERROR'} , status = 404)
