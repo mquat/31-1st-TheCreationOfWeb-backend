b05bb293cec04baeab11bded7250f8878db1de71
sangahnhan <sangahn0524@gmail.com>
2022-04-04 21:44:02 +0900

    Add: CartView get 구현

---
+++ carts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from carts.views import CartView
+
+urlpatterns = [
+    path('', CartView.as_view()),
+]
+
--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http            import JsonResponse
+from django.views           import View
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+from users.utils import login_decorator
+
+class CartView(View):
+    @login_decorator
+    def get(self,request):
+        try:
+            user  = request.user        
+            carts = Cart.objects.filter(user = user).select_related('product')
+
+            cart_list = [{
+                'id'       : cart.product.id,
+                'images'   : [image.image_url for image in cart.product.picture_set.all()],
+                'name'     : cart.product.name,
+                'price'    : cart.product.price,
+                'quantity' : cart.quantity
+            } for cart in carts]
+
+            return JsonResponse({'cart_list' : cart_list} , status = 200)
+        except ValidationError as e:
+            return JsonResponse({'message' : e.message} , status = 401)
