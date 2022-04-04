b379e7323ff7665579c86adac52cc90327b049e0
sangahnhan <sangahn0524@gmail.com>
2022-04-04 13:45:37 +0900

    에러수정

--- products/views.py
+++ products/views.py
@@ -7,8 +7,10 @@ class ProductListView(View):
     def get(self,request):
         try:
             category_id = request.GET.get('category_id' , None)
-            page_id     = int(request.GET.get('page_id' , 1) or 1)
+            page_id     = int(request.GET.get('page_id' , None))
 
+            if category_id == None:
+                return JsonResponse({'message' : 'QUERY_STRING_ERROR'} , status = 404)
             if page_id < 0:
                 return JsonResponse({'message' : 'PAGE_NUMBER_ERROR'} , status = 404)
 
@@ -19,11 +21,14 @@ class ProductListView(View):
             products    = Product.objects.filter(category_id = category_id)[offset:limit]
 
             products_list = [{
-                'name'  : product.name,
-                'price' : product.price,
-                'images': [image.image_url for image in product.picture_set.all()]
+                'id'     : product.id,
+                'name'   : product.name,
+                'price'  : product.price,
+                'images' : [image.image_url for image in product.picture_set.all()]
             } for product in products]
 
             return JsonResponse({'product_list' : products_list} , status = 200)
         except ValueError:
             return JsonResponse({'message' : 'QUERY_STRING_ERROR'} , status = 404)
+        except TypeError:
+            return JsonResponse({'message' : 'QUERY_STRING_ERRORqwer'} , status = 404)
