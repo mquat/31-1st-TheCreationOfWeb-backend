f23432d6776962e77c4959b6152006533a43508a
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-04 15:26:38 +0900

    Modify: users/urls.py 엔드포인트 수정 완료

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from .views import SignUpView, SignInView, UserView
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
-    path('/mypage', UserView.as_view())
+    path('/user', UserView.as_view())
 ]
