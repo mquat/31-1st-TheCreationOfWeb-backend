67c940d336f620838f0af405018bf30e54be8859
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-05 14:05:44 +0900

    Add: CartView DELETE method 구현

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http            import JsonResponse
 from django.views           import View
 from django.core.exceptions import ValidationError
@@ -22,3 +24,21 @@ class CartView(View):
             return JsonResponse({'cart_list' : cart_list} , status = 200)
         except ValidationError as e:
             return JsonResponse({'message' : e.message} , status = 401)
+
+    @login_decorator
+    def delete(self, request):
+        try: 
+            data = json.loads(request.body) 
+
+            user    = request.user
+            product = data['id']
+
+            if product == 0:
+                Cart.objects.filter(user=user).delete()
+                return JsonResponse({'message':'ALL_DELETED'}, status=204)
+            
+            Cart.objects.get(user=user, product=product).delete() 
+
+            return JsonResponse({'message':'ITEM_DELETED'}, status=204)
+        except ValidationError as e:
+            return JsonResponse({'message':e.message}, status=401)
