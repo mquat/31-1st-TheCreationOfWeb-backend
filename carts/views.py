84dad550b8be2c7329b649f034e0ea62515dae95
sangahnhan <sangahn0524@gmail.com>
2022-04-05 12:04:57 +0900

    no change

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.urls import View
+from django.http import JsonResponse
+
+from carts.models import Cart
+
+class CartView(View):
+    def patch(self,request):
+        # request 에는 product_id 와 quantity 들어간다
+        user = request.user
+        data = json.loads(request.body)
+        product_id = data['product_id']
+        quantity = data['quantity']
+
+        
+        #return 바뀐 quantity, 총 금액
