0824737f672f7246f2636d267a37621bfa7d8693
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-06 17:56:59 +0900

    Modify: post 삭제

--- carts/views.py
+++ carts/views.py
@@ -63,25 +63,3 @@ class CartListView(View):
             return JsonResponse({'message':'NO_CONTENT'}, status=204)
         except ValidationError as e:
             return JsonResponse({'message':e.message}, status=401)
-
-    @login_decorator
-    def post(self, request):
-        try:
-            data = json.loads(request.body)
-
-            cart, is_created = Cart.objects.get_or_create(
-                user        = request.user,  
-                product_id  = data['product_id'], 
-                defaults    = {
-                'price'    : data['price'],
-                'quantity' : data['quantity']
-                },
-            )
-
-            if not is_created:
-                cart.quantity += data['quantity']
-                cart.save() 
-
-            return JsonResponse({'message':'success'}, status=201)
-        except ValidationError as e:
-            return JsonResponse({'message':e.message}, status=401)
